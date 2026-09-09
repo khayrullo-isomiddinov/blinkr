d1936695308131ba278195ffdc59e8f74383d8b9
Khayrullo Isomiddinov <isomiddinovkhayrullo@gmail.com>
2026-09-09 14:24:53 +0200

    drop dead app.css import

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,5 +1,3 @@
-import './App.css';
-
 import HomeFeedPage from './pages/HomeFeedPage';
 import ExplorePage from './pages/ExplorePage';
 import NotificationsFeedPage from './pages/NotificationsFeedPage';
